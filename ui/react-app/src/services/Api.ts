095b73920b7ff4fffc8b4c31e751a540e6e3b8fe
workspace014-admin <oscar.osorio1884@gmail.com>
2026-02-04 18:54:13 -0500

    feat(react,services): modify services and use classes

---
+++ ui/react-app/src/services/Api.ts
@@ -0,0 +1,13 @@
+const Api = {
+    BASE_URL: import.meta.env.VITE_API_URL,
+
+    INSIGHTS: "/insights",
+
+    CREATE_CANDIDATE: "/candidate",
+    DELETE_CANDIDATE: "/candidate",
+    EDIT_CANDIDATE: "/candidate",
+    LIST_CANDIDATES: "/candidate",
+    GET_CANDIDATE: "/candidate",
+}
+
+export default Api;
