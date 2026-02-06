291abe38d42099a9e0a8a148a0bea2109a1f2880
workspace014-admin <oscar.osorio1884@gmail.com>
2026-02-06 11:34:32 -0500

    feat(react,app,camponent)improve classes in app and card

--- ui/react-app/src/App.tsx
+++ ui/react-app/src/App.tsx
@@ -4,19 +4,27 @@ import { CreatePage } from './pages/Create'
 import { EditPage } from './pages/Edit'
 import { ShowPage } from './pages/Show'
 import { ListPage } from './pages/List'
-import { Navbar } from './components/Navbar'
+import { Navbar } from './components/layout/Navbar'
+import { Sidebar } from './components/layout/Sidebar'
 
 function App() {
   return (
-    
-<>      <Navbar />
-      <Routes>
-        <Route path="/" element={<ListPage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/edit/:id" element={<EditPage />} />
-        <Route path="/:id/insight" element={<ShowPage />} />
-      </Routes>
-    </>
+    <div className='flex min-h-screen bg-slate-50'>
+      <div className='shrink-0'>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        <main className='overflow-auto p-4 md:p-6 bg-zinc-100/10 min-w-full'>
+          <Routes>
+            <Route path="/" element={<ListPage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/edit/:id" element={<EditPage />} />
+            <Route path="/:id/insight" element={<ShowPage />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
   )
 }
 
