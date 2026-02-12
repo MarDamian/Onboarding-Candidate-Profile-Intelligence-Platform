5ea243d2afb6a27b8c3c8285e132591feb25bae3
workspace014-admin <oscar.osorio1884@gmail.com>
2026-02-12 17:33:38 -0500

    fix(fastapi,schema): fix variable does not validate the score

--- services/api-fastapi/app/schemas/insight.py
+++ services/api-fastapi/app/schemas/insight.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Union
 
 class InsightSchema(BaseModel):
     summary: str = Field(description="Resumen ejecutivo del candidato")
-    score: int = Field(description="Puntaje de 0 a 100")
+    score: Union[float, int] = Field(description="Puntaje de 0 a 1 o 0 a 100")
     strengths: List[str] = Field(description="Lista de fortalezas")
     weaknesses: List[str] = Field(description="Lista de áreas de mejora")
     suggested_role: str = Field(description="Rol sugerido para el candidato")
